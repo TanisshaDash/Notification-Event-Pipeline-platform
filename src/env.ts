9f1b7b58e32e445241564b07df25391a9cfd7fa7
TanisshaDash <tanissha.1610@gmail.com>
2026-05-29 13:53:59 +0530

    fix

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,5 @@
 import { z } from "zod";
 
-
-
 const schema = z.object({
   NODE_ENV:      z.enum(["development", "production", "test"]).default("development"),
   PORT:          z.coerce.number().default(3000),
